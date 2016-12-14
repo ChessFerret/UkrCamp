8b3e80e13fb09d110e8bed4e8a77a62b8f5a2cec
Sasha <sasha.svyatets@gmail.com>
2016-12-14 18:12:57 +0200

    Style campgrounds page. + header/jumbotron. + grid

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var campgrounds = [
 		{name: "Sofiyivsky Park", image: "https://c8.staticflickr.com/1/93/246477439_5ea3e472a0_z.jpg"},
 		{name: "Buchansky Park", image: "https://c5.staticflickr.com/7/6186/6090714876_44d269ed7e_z.jpg"},
 		{name: "Pushtcha Voditsa Park", image: "https://c7.staticflickr.com/6/5319/7407436246_0ac54dd559_z.jpg"},
+		{name: "Waterfall", image: "https://farm2.staticflickr.com/1203/1132895352_afd086a60b.jpg"},
+		{name: "Buchansky Park", image: "https://c5.staticflickr.com/7/6186/6090714876_44d269ed7e_z.jpg"},
+		{name: "Pushtcha Voditsa Park", image: "https://c7.staticflickr.com/6/5319/7407436246_0ac54dd559_z.jpg"},
+		{name: "Sofiyivsky Park", image: "https://c8.staticflickr.com/1/93/246477439_5ea3e472a0_z.jpg"},
+		{name: "Buchansky Park", image: "https://c5.staticflickr.com/7/6186/6090714876_44d269ed7e_z.jpg"},
+		{name: "Pushtcha Voditsa Park", image: "https://c7.staticflickr.com/6/5319/7407436246_0ac54dd559_z.jpg"},
 ]
 
 app.get("/", function(req, res) {
